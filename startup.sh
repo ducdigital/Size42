afd75bf3c8c71ccbe9a92cab7840bed02718b84b
root <root@constantinos-GX60-1AC-GX60-3AE.(none)>
2014-07-07 20:15:28 +0200

    mondora-asteroid update

--- startup.sh
+++ startup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 MONGO_URL=mongodb://localhost:27017/meteor_dev  meteor
-=======
 #MONGO_URL=mongodb://localhost:27017/meteor_dev meteor
 #MONGO_URL=mongodb://localhost:27017/meteor_dev ROOT_URL=http://size42-111793.euw1-2.nitrousbox.com/ meteor --release 0.8.0
 #We love living on edge. Running meteor devel
-MONGO_URL=mongodb://localhost:27017/meteor_dev ROOT_URL=http://local.size42.me:3000/ ALLOWED_ORIGIN='http://local.size42.me:8100' meteor 
->>>>>>> 12033ac3f7e6687bf57cde93a0ba83069ca3bd6c
+MONGO_URL=mongodb://localhost:27017/meteor_dev ROOT_URL=http://local.size42.me:8100/ ALLOWED_ORIGIN='http://local.size42.me:8100' meteor
